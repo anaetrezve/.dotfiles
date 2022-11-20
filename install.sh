15fcd1a598d2b89517d063bc29e9b620821cb758
Anaet <anaetrezve@gmail.com>
2022-11-20 20:50:24 +0100

    install script updated and .zshrc updated

--- install.sh
+++ install.sh
@@ -1,19 +1,34 @@
 #!/usr/bin/env bash
 
-# cd ~/ ; ln -s ~/.dotfiles/zsh/.zshrc
+NVIM_CONFIG_FILE_DIR=~/.config
+TMUX_CONFIG_FILE_PATH=~/.tmux.conf
 
-NVIM_CONFIG_FILE_DIR=~/.config/
-
-function link_nvim_config_file() {
-#	ln -s ./nvim/init.lua ~/.config/nvim/init.lua
-	ln -s ~/.dotfiles/nvim/ ~/.config/
+function link_nvim_config() {
+	ln -s ~/.dotfiles/nvim ~/.config
 }
 
+function link_tmux_config() {
+  ln -s ~/.dotfiles/tmux/.tmux.conf $TMUX_CONFIG_FILE_PATH
+}
+
+# Checking if Neovim config already exists
 if [ -d $NVIM_CONFIG_FILE_DIR ]; then
-	echo "Linking nvim config folder..."
-	link_nvim_config_file;
+	printf "Neovim config setup started...\n";
+	link_nvim_config;
+	printf "Neovim config setup done \n\n";
 else 
+	printf ".config folder not found. Creating .config folder on home directory\n\n";
 	mkdir -p ~/.config/
-	link_nvim_config_file;
+	link_nvim_config;
+  printf ".config folder created and Neovim config setup done \n\n";
+fi
+
+# Checking if tmux config already exists
+if [ -f $TMUX_CONFIG_FILE_PATH ]; then
+  printf "Tmux config file already exists\n\n";
+else
+  printf "Setting up Tmux config\n";
+  link_tmux_config;
+  printf "Setting up Tmux config done\n\n";
 fi
 
