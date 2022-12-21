99ecf211a630ee57b7cd2763cacd6c27ac670c31
Anaet Hossain <anaetrezve@gmail.com>
2022-12-21 03:15:49 +0100

    tabby added

--- nvim/lua/anaet/plugins-setup.lua
+++ nvim/lua/anaet/plugins-setup.lua
@@ -61,6 +61,9 @@ return packer.startup(function(use)
 	-- use("nvim-lualine/lualine.nvim")
 	use("feline-nvim/feline.nvim")
 
+	-- tabs
+	use("nanozuki/tabby.nvim")
+
 	-- fuzzy finding w/ telescope
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
 	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
