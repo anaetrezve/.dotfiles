0c721a9e7a9a4b774a7d857ba518f6149fa1d1b3
Anaet <anaetrezve@gmail.com>
2022-12-28 00:27:28 +0100

    zsh config script added in install file

--- install.sh
+++ install.sh
@@ -3,17 +3,22 @@
 NVIM_CONFIG_FILE_DIR=~/.config
 TMUX_CONFIG_FILE_PATH=~/.tmux.conf
 ALACRITTY_CONFIG_FILE_PATH=~/.config/alacritty/alacritty.yml
+ZSH_CONFIG_FILE_PATH=~/.zshrc
 
 # Checking if brew installed then installing ripgrep
 if [ -x "$(command -v brew)" ]; then
-  brew install ripgrep;
-  wait;
+  brew install ripgrep
+  wait
 else
   echo "brew is not installed. Skip installing ripgrep..."
 fi
 
+function link_zsh_config() {
+  ln -s ~/.dotfiles/zsh/.zshrc $ZSH_CONFIG_FILE_PATH
+}
+
 function link_nvim_config() {
-	ln -s ~/.dotfiles/nvim ~/.config
+  ln -s ~/.dotfiles/nvim ~/.config
 }
 
 function link_tmux_config() {
@@ -21,41 +26,49 @@ function link_tmux_config() {
 }
 
 function link_alacritty_config() {
-	ln -s ~/.dotfiles/alacritty ~/.config
+  ln -s ~/.dotfiles/alacritty ~/.config
 }
 
 function clone_tmux_plugin_manager() {
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
+# Checking if ZSH config already exists
+if [ -f $ZSH_CONFIG_FILE_PATH]; then
+  printf "ZSH config already exists"
+else
+  printf "Creating and linking ZSH config file"
+  link_zsh_config
+  printf "Creating and linking ZSH config file completed 🥳"
+fi
+
 # Checking if Neovim config already exists
 if [ -d $NVIM_CONFIG_FILE_DIR ]; then
-	printf "Neovim config setup started...\n";
-	link_nvim_config;
-	printf "Neovim config setup done \n\n";
-else 
-	printf ".config folder not found. Creating .config folder on home directory\n\n";
-	mkdir -p ~/.config/
-	link_nvim_config;
-  printf ".config folder created and Neovim config setup done \n\n";
+  printf "Neovim config setup started...\n"
+  link_nvim_config
+  printf "Neovim config setup done \n\n"
+else
+  printf ".config folder not found. Creating .config folder on home directory\n\n"
+  mkdir -p ~/.config/
+  link_nvim_config
+  printf ".config folder created and Neovim config setup done \n\n"
 fi
 
 # Checking if tmux config already exists
 if [ -f $TMUX_CONFIG_FILE_PATH ]; then
-  printf "Tmux config file already exists\n\n";
+  printf "Tmux config file already exists\n\n"
 else
-  printf "Setting up Tmux config\n";
-  link_tmux_config;
-  clone_tmux_plugin_manager;
-  printf "Setting up Tmux config done\n\n";
+  printf "Setting up Tmux config\n"
+  link_tmux_config
+  clone_tmux_plugin_manager
+  printf "Setting up Tmux config done\n\n"
 fi
 
 # Checking if alacritty config already exists
 if [ -f $ALACRITTY_CONFIG_FILE_PATH ]; then
-  printf "Alacritty config file already exists\n\n";
+  printf "Alacritty config file already exists\n\n"
 else
-  printf "Setting up Alacritty config\n";
-  link_alacritty_config;
-  printf "Setting up Alacritty config done\n\n";
+  printf "Setting up Alacritty config\n"
+  link_alacritty_config
+  printf "Setting up Alacritty config done\n\n"
 fi
-
