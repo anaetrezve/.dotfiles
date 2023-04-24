b7dea05bc3636c4c01afd00ac707ec26fa2603bc
Anaet Hossain <anaetrezve@gmail.com>
2023-04-24 11:48:32 +0200

    new options added

--- nvim/lua/anaet/core/options.lua
+++ nvim/lua/anaet/core/options.lua
@@ -39,3 +39,51 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
+
+opt.fileencoding = "utf-8" -- the encoding written to a file
+
+opt.backup = false -- creates a backup file
+
+opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
+
+opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+
+opt.conceallevel = 0 -- so that `` is visible in markdown files
+
+opt.showtabline = 2 -- always show tabs
+
+opt.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
+
+opt.undofile = true -- enable persistent undo
+
+opt.updatetime = 300 -- faster completion (4000ms default)
+
+opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+
+opt.hlsearch = true -- highlight all matches on previous search pattern
+
+opt.scrolloff = 4 -- is one of my fav
+
+opt.sidescrolloff = 4
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_create_autocmd("ColorScheme", {
+	callback = function()
+		local highlights = {
+			"Normal",
+			"LineNr",
+			"Folded",
+			"NonText",
+			"SpecialKey",
+			"VertSplit",
+			"SignColumn",
+			"EndOfBuffer",
+			"TablineFill", -- this is specific to how I like my tabline to look like
+		}
+		for _, name in pairs(highlights) do
+			vim.cmd.highlight(name .. " guibg=none ctermbg=none")
+		end
+	end,
+})
+vim.cmd("autocmd Colorscheme * highlight NvimTreeNormal guibg=none guifg=none")
