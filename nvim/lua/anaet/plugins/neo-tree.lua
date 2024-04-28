57b4fb4d675a8a817039d7003f292a626515391b
Anaet <anaetrezve@gmail.com>
2024-04-28 03:47:28 +0200

    show buffer and git tab on neo tree

--- nvim/lua/anaet/plugins/neo-tree.lua
+++ nvim/lua/anaet/plugins/neo-tree.lua
@@ -1,73 +1,86 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    cmd = "Neotree",
-    keys = function()
-  local find_buffer_by_type = function(type)
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-      local ft = vim.api.nvim_buf_get_option(buf, "filetype")
-      if ft == type then return buf end
-    end
-    return -1
-  end
-  local toggle_neotree = function(toggle_command)
-    if find_buffer_by_type "neo-tree" > 0 then
-      require("neo-tree.command").execute { action = "close" }
-    else
-      toggle_command()
-    end
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	cmd = "Neotree",
+	keys = function()
+		local find_buffer_by_type = function(type)
+			for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+				local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+				if ft == type then
+					return buf
+				end
+			end
+			return -1
+		end
 
-  return {
-    {
-      "<leader>fe",
-      function()
-        toggle_neotree(function() require("neo-tree.command").execute { action = "focus", reveal = true, dir = vim.uv.cwd() } end)
-      end,
-      desc = "Toggle Explorer (cwd)",
-    },
-    {
-      "<leader>fE",
-      function()
-        toggle_neotree(function() require("neo-tree.command").execute { action = "focus", reveal = true } end)
-      end,
-      desc = "Toggle Explorer (root)",
-    },
-  }
-end,
+		local toggle_neotree = function(toggle_command)
+			if find_buffer_by_type("neo-tree") > 0 then
+				require("neo-tree.command").execute({ action = "close" })
+			else
+				toggle_command()
+			end
+		end
 
-    opts = {
-      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-      open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-      filesystem = {
-        bind_to_cwd = false,
-        follow_current_file = { enabled = true },
-        use_libuv_file_watcher = true,
-      },
-      window = {
-        mappings = {
-          ["<space>"] = "none",
-          ["Y"] = {
-            function(state)
-              local node = state.tree:get_node()
-              local path = node:get_id()
-              vim.fn.setreg("+", path, "c")
-            end,
-            desc = "Copy Path to Clipboard",
-          },
-        },
-      },
-      default_component_configs = {
-        indent = {
-          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-          expander_collapsed = "",
-          expander_expanded = "",
-          expander_highlight = "NeoTreeExpander",
-        },
-      },
-    },
+		return {
+			{
+				"<leader>fe",
+				function()
+					toggle_neotree(function()
+						require("neo-tree.command").execute({ action = "focus", reveal = true, dir = vim.uv.cwd() })
+					end)
+				end,
+				desc = "Toggle Explorer (cwd)",
+			},
+			{
+				"<leader>fE",
+				function()
+					toggle_neotree(function()
+						require("neo-tree.command").execute({ action = "focus", reveal = true })
+					end)
+				end,
+				desc = "Toggle Explorer (root)",
+			},
+		}
+	end,
+
+	opts = {
+		sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+		open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+		source_selector = {
+			winbar = true,
+			statusline = false,
+		},
+		filesystem = {
+			bind_to_cwd = false,
+			follow_current_file = { enabled = true },
+			use_libuv_file_watcher = true,
+		},
+		window = {
+			mappings = {
+				["<space>"] = "none",
+				["Y"] = {
+					function(state)
+						local node = state.tree:get_node()
+						local path = node:get_id()
+						vim.fn.setreg("+", path, "c")
+					end,
+					desc = "Copy Path to Clipboard",
+				},
+			},
+		},
+		default_component_configs = {
+			indent = {
+				with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+				expander_collapsed = "",
+				expander_expanded = "",
+				expander_highlight = "NeoTreeExpander",
+			},
+		},
+	},
 }
