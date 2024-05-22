d4a7429d2802bd17327006593a1f26c2c5860a46
Anaet <anaetrezve@gmail.com>
2024-05-22 10:52:12 +0200

    added mason, linter, gitsigns

--- nvim/lua/anaet/plugins/mason.lua
+++ nvim/lua/anaet/plugins/mason.lua
@@ -1,19 +1,56 @@
 return {
   "williamboman/mason.nvim",
 
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+
   opts = {
     ui = {
       icons = {
         package_pending = " ",
         package_installed = "󰄳 ",
-        package_uninstalled = " 󰚌",
+        -- package_uninstalled = " 󰚌",
+        package_uninstalled = "✗",
       },
     },
   },
 
   config = function(_, opts)
     local mason = require("mason")
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
 
     mason.setup(opts)
+
+    mason_lspconfig.setup({
+      -- list of language servers for mason to install
+      ensure_installed = {
+        "tsserver",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "svelte",
+        "lua_ls",
+        "graphql",
+        "emmet_ls",
+        "prismals",
+        "pyright",
+      },
+    })
+
+    mason_tool_installer.setup({
+      -- list of linters or formatters for mason to install
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
+      },
+    })
   end,
 }
