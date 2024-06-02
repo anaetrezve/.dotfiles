8670df4b4a024a9bfc34dbc09b51020996be125f
Anaet <anaetrezve@gmail.com>
2024-06-02 03:52:38 +0200

    updated wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,18 @@
 local wezterm = require("wezterm")
 
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
 return {
 	term = "xterm-256color",
+
+	-- color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 
 	-- CURSOR
 	cursor_blink_ease_in = "Constant",
@@ -13,16 +24,20 @@ return {
 	font_size = 15,
 	line_height = 1.4,
 	font = wezterm.font({
-		family = "CaskaydiaCove Nerd Font",
+		family = "JetBrains Mono",
 		harfbuzz_features = { "liga=1" },
 	}),
 
 	window_padding = {
-		top = 0,
+		top = 8,
 		left = 0,
 		right = 0,
 		bottom = 0,
 	},
+
+	native_macos_fullscreen_mode = false,
+	window_decorations = "RESIZE",
+	enable_tab_bar = false,
 
 	keys = {
 		{
