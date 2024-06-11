3c6cb5a5740a48eb1811866cf2e2d2cbb552189e
Anaet <anaetrezve@gmail.com>
2024-06-11 18:56:56 +0200

    added new languages for treesitter

--- nvim/lua/anaet/plugins/treesitter.lua
+++ nvim/lua/anaet/plugins/treesitter.lua
@@ -1,20 +1,53 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPost", "BufNewFile" },
-	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "luadoc",
+      "printf",
+      "vim",
+      "vimdoc",
+      "bash",
+      "css",
+      "csv",
+      "dockerfile",
+      "go",
+      "gomod",
+      "gosum",
+      "graphql",
+      "html",
+      "json",
+      "json5",
+      "jsonc",
+      "javascript",
+      "markdown_inline",
+      "gitignore",
+      "diff",
+      "git_config",
+      "comment",
+      "python",
+      "rust",
+      "scss",
+      "styled",
+      "tmux",
+      "toml",
+      "tsv",
+      "tsx",
+      "typescript",
+      "yaml",
+    },
 
-		highlight = {
-			enable = true,
-			use_languagetree = true,
-		},
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+    },
 
-		indent = { enable = true },
-	},
+    indent = { enable = true },
+  },
 
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
