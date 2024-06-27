02fb94053a2203eb005c08f7928af2a018b80de8
Anaet <anaetrezve@gmail.com>
2024-06-28 01:17:57 +0200

    trying to update cmp colors

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,3 +1,68 @@
+local function border(hl_name)
+  return {
+    { "┌", hl_name },
+    { "─", hl_name },
+    { "┐", hl_name },
+    { "│", hl_name },
+    { "┘", hl_name },
+    { "─", hl_name },
+    { "└", hl_name },
+    { "│", hl_name },
+
+    -- { "╭", hl_name },
+    -- { "─", hl_name },
+    -- { "╮", hl_name },
+    -- { "│", hl_name },
+    -- { "╯", hl_name },
+    -- { "─", hl_name },
+    -- { "╰", hl_name },
+    -- { "│", hl_name },
+  }
+end
+
+local icons = {
+  Namespace = "󰌗",
+  Text = "󰉿",
+  Method = "󰆧",
+  Function = "󰆧",
+  Constructor = "",
+  Field = "󰜢",
+  Variable = "󰀫",
+  Class = "󰠱",
+  Interface = "",
+  Module = "",
+  Property = "󰜢",
+  Unit = "󰑭",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "󰌋",
+  Snippet = "",
+  Color = "󰏘",
+  File = "󰈚",
+  Reference = "󰈇",
+  Folder = "󰉋",
+  EnumMember = "",
+  Constant = "󰏿",
+  Struct = "󰙅",
+  Event = "",
+  Operator = "󰆕",
+  TypeParameter = "󰊄",
+  Table = "",
+  Object = "󰅩",
+  Tag = "",
+  Array = "[]",
+  Boolean = "",
+  Number = "",
+  Null = "󰟢",
+  String = "󰉿",
+  Calendar = "",
+  Watch = "󰥔",
+  Package = "",
+  Copilot = "",
+  Codeium = "",
+  TabNine = "",
+}
+
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
@@ -6,12 +71,12 @@ return {
     "hrsh7th/cmp-path", -- source for file system paths
     "saadparwaiz1/cmp_luasnip", -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
+    -- "onsails/lspkind.nvim", -- vs-code like pictograms
   },
 
   config = function()
     local cmp = require("cmp")
-    local lspkind = require("lspkind")
+    -- local lspkind = require("lspkind")
 
     require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -21,26 +86,49 @@ return {
           require("luasnip").lsp_expand(args.body)
         end,
       },
+
       completion = {
-        completeopt = "menu,menuone,preview",
+        completeopt = "menu,menuone",
       },
+
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
-        format = lspkind.cmp_format({
-          -- maxwidth = 50,
-          -- ellipsis_char = "...",
-        }),
+        fields = { "kind", "abbr", "menu" },
+        format = function(_, item)
+          local icon = icons[item.kind] or ""
+          icon = " " .. icon .. " "
+          item.kind = icon
+          -- item.kind = string.format("%s %s", icon, item.kind or "")
+          -- item.menu = ""
+          -- item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
+
+          return item
+        end,
       },
 
       window = {
         completion = {
           scrollbar = false,
-          side_padding = 1,
+          side_padding = 0,
+          border = border("CmpBorder"),
+          winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
         },
-        -- documentation = {}
+
+        documentation = {
+          border = border("CmpDocBorder"),
+          winhighlight = "Normal:CmpDoc",
+        },
 
         -- completion = cmp.config.window.bordered(),
         -- documentation = cmp.config.window.bordered(),
+      },
+
+      view = {
+        entries = {
+          name = "custom",
+          selection_order = "near_cursor",
+          follow_cursor = true,
+        },
       },
 
       mapping = cmp.mapping.preset.insert({
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,31 @@
+-- Catppuccin Mocha colors palettes
+-- rosewater = "#f5e0dc",
+-- flamingo = "#f2cdcd",
+-- pink = "#f5c2e7",
+-- mauve = "#cba6f7",
+-- red = "#f38ba8",
+-- maroon = "#eba0ac",
+-- peach = "#fab387",
+-- yellow = "#f9e2af",
+-- green = "#a6e3a1",
+-- teal = "#94e2d5",
+-- sky = "#89dceb",
+-- sapphire = "#74c7ec",
+-- blue = "#89b4fa",
+-- lavender = "#b4befe",
+-- text = "#cdd6f4",
+-- subtext1 = "#bac2de",
+-- subtext0 = "#a6adc8",
+-- overlay2 = "#9399b2",
+-- overlay1 = "#7f849c",
+-- overlay0 = "#6c7086",
+-- surface2 = "#585b70",
+-- surface1 = "#45475a",
+-- surface0 = "#313244",
+-- base = "#1e1e2e",
+-- mantle = "#181825",
+-- crust = "#11111b",
+
 return {
   "catppuccin/nvim",
   lazy = false,
@@ -17,6 +45,54 @@ return {
         -- which_key = true,
         -- lsp_trouble = true,
       },
+
+      custom_highlights = function(colors)
+        return {
+          -- Cmp Menu
+          -- CmpPmenu = { bg = colors.crust },
+          CmpBorder = { fg = colors.overlay2 },
+          CmpSel = { link = "PmenuSel", bold = true },
+          -- CmpItemMenu = { fg = colors.overlay1, italic = true },
+          CmpItemAbbr = { fg = colors.subtext1 },
+          CmpItemAbbrMatch = { fg = colors.sapphire, bold = true },
+          -- CmpDoc = { bg = colors.crust },
+          CmpDocBorder = { fg = colors.overlay2 },
+
+          -- Telescope
+          TelescopeBorder = { fg = colors.blue },
+          TelescopeSelectionCaret = { fg = colors.red },
+          TelescopeSelection = { fg = colors.yellow, bg = colors.surface0, style = { "bold" } },
+          TelescopeMatching = { fg = colors.blue },
+          TelescopePromptPrefix = { fg = colors.yellow, bg = colors.crust },
+          TelescopePromptTitle = { fg = colors.crust, bg = colors.mauve, style = { "bold", "italic" } },
+          TelescopePromptNormal = { bg = colors.crust },
+          TelescopePromptBorder = { bg = colors.crust, fg = colors.crust },
+          TelescopeResultsNormal = { bg = colors.mantle },
+          TelescopeResultsTitle = { fg = colors.crust, bg = colors.green, style = { "bold", "italic" } },
+          TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+          TelescopePreviewNormal = { bg = colors.crust },
+          TelescopePreviewBorder = { bg = colors.crust, fg = colors.crust },
+          TelescopePreviewTitle = { fg = colors.crust, bg = colors.sapphire, style = { "bold", "italic" } },
+
+          -- Cursorline & Linenumbers
+          -- CursorLine = { bg = colors.mantle },
+          -- Cursor = { bg = colors.red },
+
+          -- Neotree
+          NeoTreeRootName = { style = { "bold", "italic" } },
+          NeoTreeGitAdded = { fg = colors.green, style = { "italic" } },
+          NeoTreeGitConflict = { fg = colors.red, style = { "italic" } },
+          NeoTreeGitDeleted = { fg = colors.red, style = { "italic" } },
+          NeoTreeGitIgnored = { fg = colors.overlay0, style = { "italic" } },
+          NeoTreeGitModified = { fg = colors.yellow, style = { "italic" } },
+          NeoTreeGitUnstaged = { fg = colors.red, style = { "italic" } },
+          NeoTreeGitUntracked = { fg = colors.teal, style = { "italic" } },
+          NeoTreeGitStaged = { fg = colors.green, style = { "italic" } },
+
+          -- Treesitter Context
+          TreesitterContext = { bg = colors.mantle },
+        }
+      end,
     }
   end,
 
