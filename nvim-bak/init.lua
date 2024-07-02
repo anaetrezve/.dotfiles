0a50d791cbf4eb884cd0ce52edeb4e736b70759b
Anaet <anaetrezve@gmail.com>
2024-07-02 15:47:41 +0200

    remove unnecessary files

--- nvim-bak/init.lua
+++
@@ -1,2 +0,0 @@
-require("anaet.core")
-require("anaet.lazy")
--- nvim-bak/lua/anaet/plugins/common.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "nvim-lua/plenary.nvim", -- plugin that many plugins depends on
-  "christoomey/vim-tmux-navigator", -- vim & tmux window navigation plugin
-}
