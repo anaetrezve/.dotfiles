5b41574b4a786a7383c1ec973e0885c590efb720
Anaet Hossain Rezve <anaethossain.rezve@doctolib.com>
2024-10-24 10:57:30 +0200

    navigate tab or buffer keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -78,8 +78,25 @@ keymap("n", "<ESC>", function()
   end
 end)
 
-keymap("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- Set the keymap
+keymap("n", "<leader>tn", function()
+  if vim.fn.tabpagenr("$") > 1 then
+    vim.cmd("tabnext")
+  else
+    vim.cmd("bnext")
+  end
+end, { desc = "Go to next tab or buffer" })
+
+keymap("n", "<leader>tp", function()
+  if vim.fn.tabpagenr("$") > 1 then
+    vim.cmd("tabprev")
+  else
+    vim.cmd("bprev")
+  end
+end, { desc = "Go to previous tab or buffer" })
+
+-- keymap("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+-- keymap("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+-- keymap("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+-- keymap("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+-- keymap("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
