dfa4a24965068830fc0741ef2dc33c4432277d41
Anaet Hossain Rezve <anaethossain.rezve@doctolib.com>
2025-02-19 11:41:53 +0100

    ruby lsp wip

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -42,7 +42,7 @@ return {
       conform.format({
         lsp_fallback = true,
         async = false,
-        timeout_ms = 500,
+        -- timeout_ms = 500,
       })
     end, { desc = "Format file or range (in visual mode)" })
   end,
--- nvim/lua/plugins/lsp/servers.lua
+++ nvim/lua/plugins/lsp/servers.lua
@@ -1,4 +1,17 @@
 -- Servernames added from https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
+local function get_ruby_lsp_path()
+  local handle = io.popen("rbenv which ruby-lsp")
+  local result = handle:read("*a")
+  handle:close()
+  return result:gsub("%s+", "")
+end
+
+local function get_solargraph_path()
+  local handle = io.popen("rbenv which solargraph")
+  local result = handle:read("*a")
+  handle:close()
+  return result:gsub("%s+", "")
+end
 
 return {
   lua_ls = {
@@ -20,30 +33,29 @@ return {
 
   gopls = {},
 
-  -- ruby_lsp = {
-  -- cmd_env = { BUNDLE_GEMFILE = vim.fn.getenv("GLOBAL_GEMFILE") },
-  -- cmd = { "ruby-lsp" },
-  -- filetypes = { "ruby", "eruby" },
-  -- root_dir = function()
-  --   return vim.loop.cwd()
-  -- end,
-  -- },
-
-  solargraph = {
-    -- filetypes = { 'ruby' },
-    -- settings = {
-    --   solargraph = {
-    --     commandPath = { os.getenv("HOME") .. "/.rbenv/shims/solargraph", "stdio" },
-    --     autoformat = true,
-    --     completion = true,
-    --     diagnostic = true,
-    --     folding = true,
-    --     references = true,
-    --     rename = true,
-    --     symbols = true,
+  ruby_lsp = {
+    -- cmd_env = { BUNDLE_GEMFILE = vim.fn.getenv("GLOBAL_GEMFILE") },
+    -- cmd = { "ruby-lsp" },
+    -- filetypes = { "ruby", "eruby" },
+    -- root_dir = function()
+    --   return vim.loop.cwd()
+    -- end,
+    --   cmd = { get_ruby_lsp_path(), "stdio" },
+    --   settings = {
+    --     ruby_lsp = {
+    --       diagnostics = true,
+    --     },
     --   },
-    -- },
   },
+
+  -- solargraph = {
+  -- cmd = { get_solargraph_path(), "stdio" },
+  -- settings = {
+  --   solargraph = {
+  --     diagnostics = true,
+  --   },
+  -- },
+  -- },
 
   jsonls = {},
 
