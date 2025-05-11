c52eec15f0f06152e765f32c74f11874af854241
Anaet <anaetrezve@gmail.com>
2025-05-12 01:27:32 +0200

    improved options

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,16 @@
+local keymap = vim.keymap.set
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Keymap for opening file explorer (Neo Tree or native)
+keymap("n", "<leader>e", function()
+  local has_neo_tree, _ = pcall(require, "neo-tree")
+  if has_neo_tree then
+    vim.cmd("Neotree toggle")
+  else
+    vim.cmd("Explore")
+  end
+end, { desc = "Toggle File Explorer (Neo Tree/Native)" })
