fd68948a382984e8c4204a01011fb40219f2ac83
Anaet <anaetrezve@gmail.com>
2025-06-01 19:59:10 +0200

    lazydev and blink added

--- nvim/lua/plugins/lazydev.lua
+++ nvim/lua/plugins/lazydev.lua
@@ -8,19 +8,19 @@ return {
       },
     },
   },
-  {
-    "saghen/blink.cmp",
-    opts = {
-      sources = {
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            score_offset = 100,
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "saghen/blink.cmp",
+  --   opts = {
+  --     sources = {
+  --       default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+  --       providers = {
+  --         lazydev = {
+  --           name = "LazyDev",
+  --           module = "lazydev.integrations.blink",
+  --           score_offset = 100,
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
