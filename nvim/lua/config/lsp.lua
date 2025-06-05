3d056780024d5fe2f6d06d33e057fde61cf1e93f
Anaet <anaetrezve@gmail.com>
2025-06-06 01:20:31 +0200

    add ts_ls

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,5 +1,9 @@
-vim.lsp.enable("lus_ls")
-
 vim.lsp.config("*", {
   capabilities = require("blink.cmp").get_lsp_capabilities(),
 })
+
+-- getting all filenames from lsp directory
+-- and extracting filenames without extension and enabling lsp
+for _, v in ipairs(vim.api.nvim_get_runtime_file("lsp/*", true)) do
+  vim.lsp.enable(vim.fn.fnamemodify(v, ":t:r"))
+end
