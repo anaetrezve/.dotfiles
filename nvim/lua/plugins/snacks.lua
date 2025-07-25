f483e7dba492d969db3e6e91b5b117e8e7fc7ad4
Anaet <anaetrezve@gmail.com>
2025-07-25 17:32:19 +0200

    remove lsp from snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -444,13 +444,13 @@ return {
       end,
       desc = "Resume",
     },
-    {
-      "<leader>su",
-      function()
-        Snacks.picker.undo()
-      end,
-      desc = "Undo History",
-    },
+    -- {
+    --   "<leader>su",
+    --   function()
+    --     Snacks.picker.undo()
+    --   end,
+    --   desc = "Undo History",
+    -- },
     {
       "<leader>uC",
       function()
@@ -459,42 +459,42 @@ return {
       desc = "Colorschemes",
     },
     -- LSP
-    {
-      "gd",
-      function()
-        Snacks.picker.lsp_definitions()
-      end,
-      desc = "Goto Definition",
-    },
-    {
-      "gD",
-      function()
-        Snacks.picker.lsp_declarations()
-      end,
-      desc = "Goto Declaration",
-    },
-    {
-      "gr",
-      function()
-        Snacks.picker.lsp_references()
-      end,
-      nowait = true,
-      desc = "References",
-    },
-    {
-      "gI",
-      function()
-        Snacks.picker.lsp_implementations()
-      end,
-      desc = "Goto Implementation",
-    },
-    {
-      "gy",
-      function()
-        Snacks.picker.lsp_type_definitions()
-      end,
-      desc = "Goto T[y]pe Definition",
-    },
+    -- {
+    --   "gd",
+    --   function()
+    --     Snacks.picker.lsp_definitions()
+    --   end,
+    --   desc = "Goto Definition",
+    -- },
+    -- {
+    --   "gD",
+    --   function()
+    --     Snacks.picker.lsp_declarations()
+    --   end,
+    --   desc = "Goto Declaration",
+    -- },
+    -- {
+    --   "gr",
+    --   function()
+    --     Snacks.picker.lsp_references()
+    --   end,
+    --   nowait = true,
+    --   desc = "References",
+    -- },
+    -- {
+    --   "gI",
+    --   function()
+    --     Snacks.picker.lsp_implementations()
+    --   end,
+    --   desc = "Goto Implementation",
+    -- },
+    -- {
+    --   "gy",
+    --   function()
+    --     Snacks.picker.lsp_type_definitions()
+    --   end,
+    --   desc = "Goto T[y]pe Definition",
+    -- },
     {
       "<leader>ss",
       function()
